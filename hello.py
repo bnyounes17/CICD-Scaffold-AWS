e56004a77e2ea67f2b06947af464b95297ad7601
Younes Bensassi-Nour <bensassi.younes@gmail.com>
2025-11-10 03:54:43 +0000

    add multiply function

--- hello.py
+++ hello.py
@@ -8,7 +8,13 @@ print("Hello!")
 def add(x, y):
     return x + y
 
+def multiply(x, y):
+    return x*y
+
 a = 1
 b = 2
 result = add(a, b)
 print(f"This is the sum: {a}, {b}, {result}")
+
+result2 = multiply(a, b)
+print(f"This is the multiplication: {a}, {b}, {result2}")
